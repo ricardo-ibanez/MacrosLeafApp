86029eedaf909f7704a35a8bbaa77d570322539d
Hugoag03 <hugoarevalogil08@gmail.com>
2024-05-22 10:13:38 +0200

    String nombre(Hugo)

--- TJavaComun/src/tjavacomun/TJavaComun.java
+++ TJavaComun/src/tjavacomun/TJavaComun.java
@@ -15,6 +15,8 @@ public class TJavaComun {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String nombre;
+        nombre = "Hugo";
     }
     
 }
