40e1d567045e935f6b5160e1480f14737592b3ec
ricardo-ibanez <ricardoibanezmejias@gmail.com>
2024-05-22 09:57:24 +0200

    Comentario Ricardo

--- TJavaComun/src/tjavacomun/TJavaComun.java
+++ TJavaComun/src/tjavacomun/TJavaComun.java
@@ -16,6 +16,8 @@ public class TJavaComun {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Soy el de la gráfica interactiva");
+        System.out.println("Soy el cabezon de los jueguitos");
+        
     }
     
 }
