b4e11f83dcbe658dcdaf946d763b9ed931f2a959
ricardo-ibanez <ricardoibanezmejias@gmail.com>
2024-05-22 10:20:20 +0200

    Creacion Class Ricardo

---
+++ TJavaComun/src/tjavacomun/Ricardo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tjavacomun;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ricardo {
+    
+    //Esta es la clase de Ricardo creada en la Rama Ricardo y Mergeada con la master
+}
